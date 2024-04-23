d8d7bb53e4ade5dc52ce94f7740e21c330ab07dc
Vasily Markov <mvv1993Vasya@gmail.com>
2024-04-23 12:05:34 +0300

    combine

--- shapes.py
+++ shapes.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import math
 from shapely.geometry import LineString, Polygon
+from scipy.spatial import ConvexHull
 
 class MyPolygon:
     def __init__(self, points):
@@ -18,6 +20,7 @@ class MyPolygon:
         self.max_y = np.max(self.points[:, 1])
         self.height = self.max_y-self.min_y
         self.width = self.max_x-self.min_x
+        self.mass_center = (np.mean(self.points[:, 0]), np.mean(self.points[:, 1]))
 
     def move_x(self, x):
         self.points[:, 0] += x
@@ -60,20 +63,48 @@ def intersection(polygon, line):
     points = list(intersection.coords)
     return points 
 
+def min_distance(poly1, poly2):
+    dist = []
+    for i in range(poly1.points.shape[0]):
+        for j in range(poly2.points.shape[0]):
+            dist.append(math.hypot(poly1.points[i, 0]-poly2.points[j, 0], 
+                              poly1.points[i, 1]-poly2.points[j, 1]))
+    min_dist = np.min(np.array(dist))
+    return min_dist
 
-def classification(global_poly, lines, poly, step, traversal='horizontal'):
+
+def combine(poly1, poly2):
+    points = np.vstack((poly1.points, poly2.points)) 
+    hull = ConvexHull(points)
+    vertices = hull.vertices
+    bounding_polygon = MyPolygon(points[vertices])
+    return bounding_polygon 
+
+# def find_and_combine_close_polygons(polygons, treshold):
+#     for i in range(polygons.shape[0]):
+#         for j in range(1, polygons.shape[0]):
+#             if min_distance(polygons[i], polygons[j]) < treshold:
+
+
+def classification(global_poly, lines, polygons, step, traversal='horizontal'):
+    polygons = polygons.copy()
     treshold = 0
-    if traversal == 'vertical':
-        diffs_min = np.abs(poly.min_x - lines[0,0,:])
-        diffs_max = np.abs(poly.max_x - lines[0,0,:])
-        closest_line_left = lines[:, :, np.argmin(diffs_min)]
-        closest_line_right= lines[:, :, np.argmin(diffs_max)]
-        is_crossing_left = poly.min_x - closest_line_left[0][0] <= treshold 
-        is_crossing_right = closest_line_right[0][0] - poly.max_x <= treshold 
-        if is_crossing_left == False and is_crossing_right == False:
-            return 'A class'
-    if traversal == 'horizontal':
-        ...
+    classified = {'a': [], 'b': [], 'c': [], 'd': []}
+    index = 0
+    for poly in polygons:
+        if traversal == 'vertical':
+            diffs_min = np.abs(poly.min_x - lines[0,0,:])
+            diffs_max = np.abs(poly.max_x - lines[0,0,:])
+            closest_line_left = lines[:, :, np.argmin(diffs_min)]
+            closest_line_right= lines[:, :, np.argmin(diffs_max)]
+            is_crossing_left = poly.min_x - closest_line_left[0][0] <= treshold 
+            is_crossing_right = closest_line_right[0][0] - poly.max_x <= treshold 
+            if is_crossing_left == False and is_crossing_right == False:
+                classified['a'].append(polygons.pop(index))
+        if traversal == 'horizontal':
+            ...
+        index += 1
+    return classified
 
 
 def plot_lines(plot, lines):
@@ -89,36 +120,51 @@ def plot_intersection(plot, intersects):
         ax.scatter(*zip(*point), color = 'r', s = 15)
 
 
-a_poly = MyPolygon([[105,100], 
-                    [102,120], 
-                    [105,140], 
-                    [108,120]])
-
 glob_poly = MyPolygon([[0,0], 
                        [20,300], 
                        [300,350], 
                        [320,0]])
 
-polygons = {
-  "glob": glob_poly,
-  "a": a_poly,
-}
+a_poly = MyPolygon([[105,100], 
+                    [102,120], 
+                    [105,140], 
+                    [108,120]])
 
-lines = generates_rays(polygons['glob'].points, 10, traversal='vertical')
+b_poly = MyPolygon([[200,200], 
+                    [200,225], 
+                    [225,225], 
+                    [225,200]])
+
+c_poly = MyPolygon([[233,200], 
+                    [233,225], 
+                    [258,225], 
+                    [258,200]])
+
+
+c_poly.move_y(20)
+
+polygons = [a_poly, b_poly, c_poly]
+
+lines = generates_rays(glob_poly.points, 10, traversal='vertical')
 
 intersect_points = []
 for i in range(lines.shape[2]):
-    intersect_points.append(intersection(polygons['glob'].points, lines[:, :, i]))
+    intersect_points.append(intersection(glob_poly.points, lines[:, :, i]))
 
 
-polygons['a'].move_x(5)
+print(classification(glob_poly, lines, polygons, step=10, traversal='vertical'))
 
-print(classification(polygons['glob'], lines, polygons['a'], step=10, traversal='vertical'))
-
-fig, ax = plt.subplots()
-ax.add_patch(patches.Polygon(polygons['glob'].points, fill=False))
-ax.add_patch(patches.Polygon(polygons['a'].points, color = 'black', fill=True))
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot()
+ax.add_patch(patches.Polygon(glob_poly.points, fill=False))
+for poly in polygons:
+    ax.add_patch(patches.Polygon(poly.points, color = 'black', fill=True))
+    
+ax.add_patch(patches.Polygon(combine(b_poly, c_poly).points, color = 'black', fill=True))
 
 plot_lines(ax, lines)
 plot_intersection(ax, intersect_points)
+
+ax.set_xlim([0, 360])
+ax.set_ylim([0, 360])
 plt.show()
