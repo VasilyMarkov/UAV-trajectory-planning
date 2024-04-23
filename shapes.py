f7f75ae1939596b917a2c3f31a89ce648da2135a
Vasily Markov <mvv1993Vasya@gmail.com>
2024-04-23 15:56:31 +0300

    Fix

--- shapes.py
+++ shapes.py
@@ -65,12 +65,41 @@ def intersection(polygon, line):
 
 def min_distance(poly1, poly2):
     dist = []
+    dist_x = []
+    dist_y = []
     for i in range(poly1.points.shape[0]):
         for j in range(poly2.points.shape[0]):
             dist.append(math.hypot(poly1.points[i, 0]-poly2.points[j, 0], 
                               poly1.points[i, 1]-poly2.points[j, 1]))
+            dist_x.append(np.abs(poly1.points[i, 0]-poly2.points[j, 0]))
+            dist_y.append(np.abs(poly1.points[i, 1]-poly2.points[j, 1]))
     min_dist = np.min(np.array(dist))
-    return min_dist
+    min_dist_x = np.min(np.array(dist_x))
+    min_dist_y = np.min(np.array(dist_y))
+    return min_dist_x, min_dist_y
+
+
+'''
+An obstacle that due to size and conﬁguration in relation 
+to the driving direction does not affect the coverage plan generation.
+'''
+def a_class(polygons, lines, threshold, traversal='vertical'):
+    index = 0
+    output = []
+    for poly in polygons:
+        if traversal == 'vertical':
+            diffs_min = np.abs(poly.min_x - lines[0,0,:])
+            diffs_max = np.abs(poly.max_x - lines[0,0,:])
+            closest_line_left = lines[:, :, np.argmin(diffs_min)]
+            closest_line_right= lines[:, :, np.argmin(diffs_max)]
+            is_crossing_left = poly.min_x - closest_line_left[0][0] <= threshold 
+            is_crossing_right = closest_line_right[0][0] - poly.max_x <= threshold 
+            if is_crossing_left == False and is_crossing_right == False:
+                output.append(polygons.pop(index))
+        if traversal == 'horizontal':
+            ...
+        index += 1
+    return output
 
 
 def combine(poly1, poly2):
@@ -80,30 +109,61 @@ def combine(poly1, poly2):
     bounding_polygon = MyPolygon(points[vertices])
     return bounding_polygon 
 
-# def find_and_combine_close_polygons(polygons, treshold):
-#     for i in range(polygons.shape[0]):
-#         for j in range(1, polygons.shape[0]):
-#             if min_distance(polygons[i], polygons[j]) < treshold:
 
-
-def classification(global_poly, lines, polygons, step, traversal='horizontal'):
+def find_and_combine_close_polygons(polygons, threshold):
+    # combined_poly = []
+    # output = []
+    # for i in range(len(polygons)):
+    #     combined = False
+    #     for j in range(1, len(polygons)):
+    #         if min_distance(polygons[i], polygons[j]) < threshold:
+    #             poly = combine(polygons.pop(i), polygons.pop(j))
+    #             # print(poly)
+    #             combined_poly.append(poly)
+                
+    #             combined = True
+    #     if combined == False:
+    #         output.append(polygons[i])
+    # print(output)
+    # return output.append(combined)
     polygons = polygons.copy()
-    treshold = 0
+    used = []
+    output = []
+    uav_size = 10
+    
+    for i in range(len(polygons)):
+        if (i in used) == False:
+            for j in range(1, len(polygons)):
+                if i != j:
+                    dist_x, dist_y = min_distance(polygons[i], polygons[j])
+                    if dist_x <= threshold and dist_y <= 1.5*uav_size:
+                        # print(dist_x, dist_y)
+                        output.append(combine(polygons[i], polygons[j]))
+                        used.append(i)
+                        used.append(j)
+                        print(used)
+    
+    return polygons
+
+
+
+'''                 
+Obstacles where the minimum distance between another obstacle is less than the operating width,
+w. In this case both obstacles are classiﬁed as of type C and a subroutine is used to ﬁnd the minimal bounding polygon (MBP) to
+enclose these obstacles.
+'''
+def c_class(polygons, threshold, traversal='vertical'):
+    return find_and_combine_close_polygons(polygons, threshold)
+
+
+def classification(global_poly, lines, polygons, step, traversal='vertical'):
+    polygons = polygons.copy()
+    threshold = 10
     classified = {'a': [], 'b': [], 'c': [], 'd': []}
-    index = 0
-    for poly in polygons:
-        if traversal == 'vertical':
-            diffs_min = np.abs(poly.min_x - lines[0,0,:])
-            diffs_max = np.abs(poly.max_x - lines[0,0,:])
-            closest_line_left = lines[:, :, np.argmin(diffs_min)]
-            closest_line_right= lines[:, :, np.argmin(diffs_max)]
-            is_crossing_left = poly.min_x - closest_line_left[0][0] <= treshold 
-            is_crossing_right = closest_line_right[0][0] - poly.max_x <= treshold 
-            if is_crossing_left == False and is_crossing_right == False:
-                classified['a'].append(polygons.pop(index))
-        if traversal == 'horizontal':
-            ...
-        index += 1
+    classified['a'] = a_class(polygons, lines, threshold, traversal)
+    # classified['b'].append(b_class(polygons, lines, traversal)) 
+    classified['c'] = c_class(polygons, threshold, traversal) 
+    # classified['d'].append(d_class(polygons, lines, traversal)) 
     return classified
 
 
@@ -126,9 +186,9 @@ glob_poly = MyPolygon([[0,0],
                        [320,0]])
 
 a_poly = MyPolygon([[105,100], 
-                    [102,120], 
+                    [103,120], 
                     [105,140], 
-                    [108,120]])
+                    [107,120]])
 
 b_poly = MyPolygon([[200,200], 
                     [200,225], 
@@ -141,8 +201,9 @@ c_poly = MyPolygon([[233,200],
                     [258,200]])
 
 
-c_poly.move_y(20)
-
+c_poly.move_y(12)
+c_poly.move_x(-6)
+a_poly.move_x(5)
 polygons = [a_poly, b_poly, c_poly]
 
 lines = generates_rays(glob_poly.points, 10, traversal='vertical')
@@ -152,16 +213,16 @@ for i in range(lines.shape[2]):
     intersect_points.append(intersection(glob_poly.points, lines[:, :, i]))
 
 
-print(classification(glob_poly, lines, polygons, step=10, traversal='vertical'))
-
+marked_obstacles = classification(glob_poly, lines, polygons, step=10, traversal='vertical')
+print(marked_obstacles)
 fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot()
 ax.add_patch(patches.Polygon(glob_poly.points, fill=False))
-for poly in polygons:
-    ax.add_patch(patches.Polygon(poly.points, color = 'black', fill=True))
-    
-ax.add_patch(patches.Polygon(combine(b_poly, c_poly).points, color = 'black', fill=True))
 
+for values in marked_obstacles.values():
+    for obstacles in values:
+        ax.add_patch(patches.Polygon(obstacles.points, color = 'black', fill=True))
+    
 plot_lines(ax, lines)
 plot_intersection(ax, intersect_points)
 
